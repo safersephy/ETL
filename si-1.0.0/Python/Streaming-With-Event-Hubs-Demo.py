619b7acee459e1860a9e559811558ae3dee96316
safersephy <safersephy>
2022-02-18 16:44:26 +0000

    updated notebook

--- si-1.0.0/Python/Streaming-With-Event-Hubs-Demo.py
+++ si-1.0.0/Python/Streaming-With-Event-Hubs-Demo.py
@@ -303,47 +303,13 @@ for stream in spark.streams.active:
 
 # COMMAND ----------
 
-# test-addcol.py
-
-from dbxdemo.spark import get_spark
-from dbxdemo.appendcol import with_status
-
-
-source_data = [
-    ("pete", "pan", "peter.pan@databricks.com"),
-    ("jason", "argonaut", "jason.argonaut@databricks.com")
-]
-source_df = get_spark().createDataFrame(
-    source_data,
-    ["first_name", "last_name", "email"]
-)
-
-actual_df = with_status(source_df)
-
-expected_data = [
-    ("pete", "pan", "peter.pan@databricks.com", "checked"),
-    ("jason", "argonaut", "jason.argonaut@databricks.com", "checked")
-]
-expected_df = get_spark().createDataFrame(
-    expected_data,
-    ["first_name", "last_name", "email", "status"]
-)
-
-assert(expected_df.collect() == actual_df.collect())
 
         
     
 
 # COMMAND ----------
 
-from pyspark.sql import SparkSession
-from functools import lru_cache
 
-@lru_cache(maxsize=None)
-def get_spark():
-    return (SparkSession.builder
-                .appName("dbxdemo")
-                .getOrCreate())
 
 # COMMAND ----------
 
