d9b8ef5633269ccfc4e9b236a49513018d9ae880
safersephy <safersephy>
2022-02-18 11:51:27 +0000

    fix2

---
+++ libraries/python/dbxdemo/test_appendcol.py
@@ -0,0 +1,10 @@
+import pytest
+
+from dbxdemo.spark import get_spark
+from dbxdemo.appendcol import with_status
+
+class TestAppendCol(object):
+
+    def test_with_status(self):
+        source_data = [
+           
